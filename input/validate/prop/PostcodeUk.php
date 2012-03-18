2a6966dfaedb035764f34017a18d4922411068e2
gordonmcvey <gordonmcvey@10.0.1.200>
2012-03-18 09:21:45 +0000

    Added UK postcode validator

---
+++ input/validate/prop/PostcodeUk.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Reefknot framework
+ * 
+ * @copyright Gordian Solutions and Gordon McVey
+ * @license http://www.apache.org/licenses/LICENSE-2.0.txt Apache license V2.0
+ */
+
+namespace gordian\reefknot\input\validate\prop;
+
+use
+	gordian\reefknot\input\validate\abstr, 
+	gordian\reefknot\input\validate\iface;
+
+/**
+ * Test whether the data is in a format compatible with UK postal code format
+ *
+ * @author gordonmcvey
+ */
+class PostcodeUk extends abstr\Prop implements iface\Prop
+{
+	const 
+	
+		/**
+		 * Defines the regular expression against which to test postal code, 
+		 * with additions so it matches the postcode only, rejecting cases where
+		 * a valid post code is wrapped in padding
+		 * 
+		 * @see http://en.wikipedia.org/wiki/Postcodes_in_the_United_Kingdom#Validation UK postal code validation rules on Wikipedia 
+		 */
+		PATTERN	= '/^(?:(GIR 0AA)|(((A[BL]|B[ABDHLNRSTX]?|C[ABFHMORTVW]|D[ADEGHLNTY]|E[HNX]?|F[KY]|G[LUY]?|H[ADGPRSUX]|I[GMPV]|JE|K[ATWY]|L[ADELNSU]?|M[EKL]?|N[EGNPRW]?|O[LX]|P[AEHLOR]|R[GHM]|S[AEGKLMNOPRSTY]?|T[ADFNQRSW]|UB|W[ADFNRSV]|YO|ZE)[1-9]?[0-9]|((E|N|NW|SE|SW|W)1|EC[1-4]|WC[12])[A-HJKMNPR-Y]|(SW|W)([2-9]|[1-9][0-9])|EC[1-9][0-9]) [0-9][ABD-HJLNP-UW-Z]{2}))\z/';
+	
+	/**
+	 *
+	 * @return bool True if valid
+	 * @throws \InvalidArgumentException 
+	 */
+	public function isValid ()
+	{
+		$valid	= false;
+		$data	= $this -> getData ();
+		
+		switch (gettype ($data))
+		{
+			case 'NULL'		:
+				$valid	= true;
+			break;
+			case 'string'	:
+				$valid	= preg_match (static::PATTERN, $data) > 0;
+			break;
+			default			:
+				throw new \InvalidArgumentException (__CLASS__ . ': This property cannot be applied to data of type ' . gettype ($data));
+			break;
+		}
+		
+		return ($valid);
+	}
+}
