06c26baab3cffac39b8d93fbb99a287ef60584bd
gordonmcvey <gordon.mcvey@ntlworld.com>
2012-03-20 14:30:47 +0000

    Unit test updates

--- _unittest/bootstrap.php
+++ _unittest/bootstrap.php
@@ -12,7 +12,11 @@
 
 // put your code here
 
+// This must be in place, as the session class is unable to start the session once output has been sent
+ob_start ();
+
 require (realpath (__DIR__ . '/../bootstrap.php'));
 
+// Storing the autoloader to a var lets us turn it off for unit testing the autoloader
 $unitTestAutoloader	= new gordian\reefknot\autoload\Autoload ();
 ?>
