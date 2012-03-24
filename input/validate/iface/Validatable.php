18ba21ad0148cea8fc8e0828e23880e45ce467c6
gordonmcvey <gordonmcvey@10.0.1.200>
2012-03-24 10:00:59 +0000

    Consolidated BasicValidatable into Validatable

--- input/validate/iface/Validatable.php
+++ input/validate/iface/Validatable.php
@@ -18,7 +18,7 @@ namespace gordian\reefknot\input\validate\iface;
  * 
  * @author gordonmcvey
  */
-interface Validatable extends BasicValidatable
+interface Validatable
 {
 	/**
 	 * Set the data to be validated
@@ -34,4 +34,50 @@ interface Validatable extends BasicValidatable
 	 * @return mixed
 	 */
 	public function getData ();
+	
+	/**
+	 * Set the parent element of the validatable
+	 * 
+	 * When a validatable is added to a container (A dataset), its parent 
+	 * reference should be set back to the set to which it was added. 
+	 * 
+	 * @param DataSet $set
+	 * @return Validatable The item should return itself so chaining can occur
+	 * @throws \InvalidArgumentException
+	 */
+	public function setParent (Field $set);
+	
+	/**
+	 * Get the parent element of the validatable
+	 * 
+	 * This method returns the parent valadatable, or NULL if this is the root
+	 * valadatable. 
+	 * 
+	 * @return Validatable
+	 */
+	public function getParent ();
+	
+	/**
+	 * Test whether the provided data is valid against the provided rules
+	 * 
+	 * @return bool True if the form is valid
+	 */
+	public function isValid ();
+	
+	/**
+	 * Test whether the last run of isValid() resulted in validation errors
+	 * 
+	 * @return bool True if the last attempt to validate the assigned data failed
+	 */
+	public function hasInvalids ();
+	
+	/**
+	 * Return a list of reasons why validation failed
+	 * 
+	 * This method returns a list of all the validatables that indicated that 
+	 * the data they were given to process was invalid
+	 * 
+	 * @return array
+	 */
+	public function getInvalids ();
 }
