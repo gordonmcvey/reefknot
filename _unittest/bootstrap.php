4a1385c3d78ca4d50e10831215e8eb5ab7ae72ff
gordonmcvey <gordonmcvey@10.0.1.200>
2012-06-20 23:02:16 +0100

    Removed ob_start call

--- _unittest/bootstrap.php
+++ _unittest/bootstrap.php
@@ -4,18 +4,7 @@
  * @author gordonmcvey
  */
 
-
-/* 
- * The following is necessary to make testing the Session class possible.  I 
- * tried @outputBuffering enabled on the test class, but it didn't seem to help.
- * 
- * I'm still looking for a better solution to this particular problem.  Any
- * suggestions are more than welcome. 
- */
-ob_start ();
-
 require (realpath (__DIR__ . '/../bootstrap.php'));
 
 // Storing the autoloader to a var lets us turn it off for unit testing the autoloader
 $unitTestAutoloader	= new gordian\reefknot\autoload\Autoloader ();
-
