391d749ec117c874cf00827e524f3090b07af47f
gordonmcvey <gordonmcvey@10.0.1.200>
2012-07-01 08:19:43 +0100

    Minor cleanup of bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -14,9 +14,8 @@ namespace gordian\reefknot;
  * This file contains the code needed to bootstrap the framework's autoloader.
  * Including this file in your code will set up some parameters the autoloader
  * depends on to work and makes the autoloader class and its associated 
- * interface available for use.  It does not activate the autoloader, you need
- * to do that yourself by creating an instance of the autoloader class. This 
- * will put the autoloader into the SPL autoload stack. 
+ * interface available for use.  It does not instantiate an autoloader instance, 
+ * you need to do that yourself by calling new Autoloader (). 
  * 
  * @author Gordon McVey
  * @category Reefknot
@@ -26,30 +25,26 @@ namespace gordian\reefknot;
 const
 
 	/**
-	 * Shorthand for PHP's DIRECTORY_SEPERATOR constant 
+	 * Shorthand for PHP's DIRECTORY_SEPERATOR constant.
 	 */
 	DS		= DIRECTORY_SEPARATOR,
 	
 	/**
-	 * Framework root namespace 
+	 * Framework root namespace.
 	 */
-	NS_FW	= __NAMESPACE__;
+	NS_FW	= __NAMESPACE__, 
+	
+	/**
+	* Define the character used as the namespace seperator.
+	*/
+	NS_SEP	= '\\';
 
 /**
  * Define the root path for the framework.  This can be overridden by defining
  * it before including the bootstrap 
  */
-defined (NS_FW . '\PATH_FW')
-	|| define (NS_FW . '\PATH_FW', realpath (__DIR__));
-
-/**
- * Define the character used as the namespace seperator.  When using PHP 5.3 
- * namespacing, this is always the backslash character, but code written before 
- * 5.3 would often use an underscore or some other character to represent a 
- * namespace 
- */
-defined (NS_FW .'\NS_SEP')
-	|| define (NS_FW .'\NS_SEP', '\\');
+defined (__NAMESPACE__ . '\PATH_FW')
+	|| define (__NAMESPACE__ . '\PATH_FW', realpath (__DIR__));
 
 // Load the autoloader
 require	( PATH_FW . DS . 'autoload' . DS . 'iface' . DS . 'Autoloader.php');
