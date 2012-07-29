f500e4f69167ca8954402e72259bc976bbde2149
gordonmcvey <gordonmcvey@10.0.1.200>
2012-07-29 11:31:34 +0100

    Putting vendor libraries in the bootstrap for using third party testing libraries such as vfsstream

--- _unittest/bootstrap.php
+++ _unittest/bootstrap.php
@@ -7,4 +7,5 @@
 require (realpath (__DIR__ . '/../bootstrap.php'));
 
 // Storing the autoloader to a var lets us turn it off for unit testing the autoloader
-$unitTestAutoloader	= new gordian\reefknot\autoload\Autoloader ();
+$unitTestAutoloader		= new \gordian\reefknot\autoload\Autoloader ();
+//$vfsStreamAutoloader	= new \gordian\reefknot\autoload\Autoloader (realpath (__DIR__ . '../../../'), 'asdf');
