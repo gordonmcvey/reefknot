441d15ff2f0b3473ebe1b0e0bddf926a196042fc
gordiansolutions <gordon.mcvey@ntlworld.com>
2014-03-20 21:52:48 +0000

    Updated unit test bootstrap

--- _unittest/bootstrap.php
+++ _unittest/bootstrap.php
@@ -5,9 +5,9 @@
  * @author Gordon McVey
  */
 
-require (realpath (__DIR__ . '/../bootstrap.php'));
-require (realpath (__DIR__ . '/../vendor/autoload.php'));
-
+require (__DIR__ . '/../bootstrap.php');
+require (__DIR__ . '/../vendor/autoload.php');
+require (__DIR__ . '/functionoverrides.php');
 use \gordian\reefknot as rf;
 
 new rf\autoload\Autoloader (rf\PATH_FW, rf\NS_FW);
